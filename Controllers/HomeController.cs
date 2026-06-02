e0e2a5b8e96a3853f3f7d66dd4662d5504cb4b8f
DELL <DELL@DESKTOP-JS9HMG9>
2026-06-02 18:51:54 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using SoftTeachInventors.Models;
+using System.Diagnostics;
+
+namespace SoftTeachInventors.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+                public IActionResult Books()
+        {
+            return View();
+        }
+
+        public IActionResult Technology()
+        {
+            return View();
+        }
+
+        public IActionResult Programs()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Views/Home/Programs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoftTeachInventors.Views.Home
+{
+    public class ProgramsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
